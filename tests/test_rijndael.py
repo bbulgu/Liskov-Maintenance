72d301ee7ee0a688fb9563a0294b3e573c89dd78
Simon Pettersson <rickebo@rickebo.com>
2022-09-08 15:35:43 +0200

    Fixed ambiguous imports and line length issues in test_rijndael.py

--- tests/test_rijndael.py
+++ tests/test_rijndael.py
@@ -1,4 +1,4 @@
-from pdfminer.rijndael import *
+from pdfminer.rijndael import RijndaelEncryptor, RijndaelDecryptor
 
 """
 Test encryption of data using key.
@@ -8,7 +8,8 @@ Test encryption of data using key.
 def test_encryptor():
     key = bytes.fromhex('00010203050607080a0b0c0d0f101112')
     plaintext = bytes.fromhex('506812a45f08c889b97f5980038b8359')
-    assert RijndaelEncryptor(key, 128).encrypt(plaintext).hex() == 'd8f532538289ef7d06b506a4fd5be9c9'
+    assert RijndaelEncryptor(key, 128).encrypt(plaintext).hex() == \
+           'd8f532538289ef7d06b506a4fd5be9c9'
 
 
 """
@@ -19,4 +20,5 @@ Test decryption of data using key.
 def test_decryptor():
     key = bytes.fromhex('00010203050607080a0b0c0d0f101112')
     ciphertext = bytes.fromhex('d8f532538289ef7d06b506a4fd5be9c9')
-    assert RijndaelDecryptor(key, 128).decrypt(ciphertext).hex() == '506812a45f08c889b97f5980038b8359'
+    assert RijndaelDecryptor(key, 128).decrypt(ciphertext).hex() == \
+           '506812a45f08c889b97f5980038b8359'
