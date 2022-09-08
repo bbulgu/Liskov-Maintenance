2ce80e057088d060e4ffa9e6a65cb2845e10506f
Simon Pettersson <rickebo@rickebo.com>
2022-09-08 15:33:15 +0200

    Fixed too long lines in psparser.py

--- pdfminer/psparser.py
+++ pdfminer/psparser.py
@@ -662,7 +662,8 @@ func/a/b{(c)do*}def
 '''
 
     TOKENS = [
-        (5, KWD(b'begin')), (11, KWD(b'end')), (16, KWD(b'"')), (19, KWD(b'@')),
+        (5, KWD(b'begin')), (11, KWD(b'end')),
+        (16, KWD(b'"')), (19, KWD(b'@')),
         (21, KWD(b'#')), (23, LIT('a')), (25, LIT('BCD')),
         (30, LIT('Some_Name')),
         (41, LIT('foo_xbaa')), (54, 0), (56, 1), (59, -2), (62, 0.5),
@@ -670,7 +671,8 @@ func/a/b{(c)do*}def
         (98, b'def \x00 4ghi'), (118, b'bach\\slask'), (132, b'foo\nbaa'),
         (143, b'this % is not a comment.'), (170, b'foo\nbaa'),
         (180, b'foobaa'),
-        (191, b''), (194, b' '), (199, b'@@ '), (211, b'\xab\xcd\x00\x124\x05'),
+        (191, b''), (194, b' '), (199, b'@@ '),
+        (211, b'\xab\xcd\x00\x124\x05'),
         (226, KWD(b'func')), (230, LIT('a')), (232, LIT('b')),
         (234, KWD(b'{')), (235, b'c'), (238, KWD(b'do*')), (241, KWD(b'}')),
         (242, KWD(b'def')), (246, KWD(b'[')), (248, 1), (250, b'z'),
@@ -686,7 +688,8 @@ func/a/b{(c)do*}def
         (98, b'def \x00 4ghi'), (118, b'bach\\slask'), (132, b'foo\nbaa'),
         (143, b'this % is not a comment.'), (170, b'foo\nbaa'),
         (180, b'foobaa'),
-        (191, b''), (194, b' '), (199, b'@@ '), (211, b'\xab\xcd\x00\x124\x05'),
+        (191, b''), (194, b' '), (199, b'@@ '),
+        (211, b'\xab\xcd\x00\x124\x05'),
         (230, LIT('a')), (232, LIT('b')), (234, [b'c']), (246, [1, b'z']),
         (258, {'foo': b'bar'}),
     ]
