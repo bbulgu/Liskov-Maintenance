a8ead289dd31e5923794bbf74b70c93dcbb0c9fe
emja9213 <Emanuel.Jansson.9213@student.uu.se>
2022-09-13 17:52:12 +0200

    Separated test results into it's own folder, which has been added to .gitignore

--- tests/pdf2txt_test.py
+++ tests/pdf2txt_test.py
@@ -26,11 +26,11 @@ def test_write_stdout_debug():
 # this test is failing because whitespaces do not match!!
 def test_write_file():
     pdf2txt.main(['pdf2txt.py', '-o',
-                  'tests/result1.txt', 'samples/simple1.pdf'])
-    with open('tests/result1.txt') as outfile_new:
+                  'tests/results/result1.txt', 'samples/simple1.pdf'])
+    with open('tests/results/result1.txt') as outfile_new:
         lines_new = outfile_new.readlines()
     oldpdf2txt.main(['pdf2txt.py', '-o',
-                     'tests/result2.txt', 'samples/simple1.pdf'])
-    with open('tests/result2.txt') as outfile_old:
+                     'tests/results/result2.txt', 'samples/simple1.pdf'])
+    with open('tests/results/result2.txt') as outfile_old:
         lines_old = outfile_old.readlines()
     assert lines_new == lines_old
