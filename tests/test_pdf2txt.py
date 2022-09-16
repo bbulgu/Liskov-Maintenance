3c37463f687948cc3968289b7cb8fec93f28c21c
fryn4538 <fredrik.yngve.4538@student.uu.se>
2022-09-16 11:06:51 +0200

    Added tests for the implementation of issue #217 Co-authored-by: emja9213 <Emanuel.Jansson.9213@student.uu.se>

--- tests/test_pdf2txt.py
+++ tests/test_pdf2txt.py
@@ -1,10 +1,12 @@
+import pytest
 from tools.pdf2txt import commandline, pdfconversion
 from pdfminer.layout import LAParams
 
 """
 Tests the commandline part of pdf2txt.py
 """
-
+def usage():
+    return 100
 
 def test_cmd():
     assert commandline(['pdf2txt.py', '-P', 'test', './sample.pdf']) == 0
@@ -39,3 +41,42 @@ def test_pdfconversion():
     assert pdfconversion(args, password, pagenos, maxpages, outfile, outtype,
                          imagewriter, rotation, stripcontrol, layoutmode,
                          encoding, scale, caching, laparams, debug) == 0
+
+def test_xml_word_empty():
+    assert commandline(['pdf2txt.py', '-t', 'xml']) == 100
+
+def test_xml_char_no_specification(capfd):
+    commandline(['pdf2txt.py', '-t', 'xml', 'samples/line.pdf'])
+    out, err = capfd.readouterr()
+
+    with open('samples/xml_output_line_char.txt', 'r') as file:
+        data = file.read()
+
+    assert out == data
+
+def test_xml_char(capfd):
+    commandline(['pdf2txt.py', '-t', 'xml|c', 'samples/line.pdf'])
+    out, err = capfd.readouterr()
+
+    with open('samples/xml_output_line_char.txt', 'r') as file:
+        data = file.read()
+
+    assert out == data
+
+def test_xml_word(capfd):
+    commandline(['pdf2txt.py', '-t', 'xml|w', 'samples/line.pdf'])
+    out, err = capfd.readouterr()
+
+    with open('samples/xml_output_line_word.txt', 'r') as file:
+        data = file.read()
+
+    assert out == data
+
+def test_xml_line(capfd):
+    commandline(['pdf2txt.py', '-t', 'xml|l', 'samples/line.pdf'])
+    out, err = capfd.readouterr()
+
+    with open('samples/xml_output_line_line.txt', 'r') as file:
+        data = file.read()
+
+    assert out == data
